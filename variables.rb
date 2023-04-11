73a44996e228440796271b0b829685761281953b
Adrian Roussev <a.roussev@mail.utoronto.ca>
2023-04-11 16:58:15 -0400

    add puts statements

--- variables.rb
+++ variables.rb
@@ -10,3 +10,10 @@ rubys_exact_age = 8.7
 adrian_over_5 = true
 camden_over_5 = false
 ruby_over_5 = true
+first_name = "not adrian"
+last_name = "not roussev"
+dogs_names = "not camden or ruby"
+puts my_age
+puts dogs_names
+puts rubys_exact_age
+puts my_age, rubys_age
